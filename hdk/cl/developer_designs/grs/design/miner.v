c5130893a85574cd68713977649aa8d5af450bf6
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-08-21 00:59:03 +0530

    Update miner.v

--- hdk/cl/developer_designs/grs/design/miner.v
+++ hdk/cl/developer_designs/grs/design/miner.v
@@ -1,100 +1,45 @@
-/*
- * Copyright (c) 2016 Sprocket
- *
- * This is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License with
- * additional permissions to the one published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version. For more information see LICENSE.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 
 module miner # (
-     parameter CORES = 32'd1
- ) (
-     input clk,
-     input reset,
-     input [639:0] block,
-     input [31:0] nonce_start,
-     output nonce_found,
-     output [31:0] nonce_out
- );
+	parameter CORES = 32'd1
+) (
+	input clk,
+	input reset,
+	input [639:0] block,
+	input [31:0] nonce_start,
+	output nonce_found,
+	output [31:0] nonce_out
+);
 
-	wire [511:0] hash1;
-    wire [511:0] hash2;
-    reg reset_d, reset_q;
-    reg [647:0] block0_d, block0_q;
-    reg [647:0] block1_d, block1_q;
-    reg [31:0] target_d, target_q;
-    reg [31:0] nonce_d, nonce_q;
-    reg [31:0] nonce_out_d, nonce_out_q;
-    reg [31:0] hash_out_d, hash_out_q;
-    reg nonce_found_d, nonce_found_q;
-    
-    assign nonce_found = nonce_found_q;
-    assign nonce_out = nonce_out_q;
-    
-	groestl512 groestl_1 ( clk, block0_q, hash1 );
-	groestl512 groestl_2 ( clk, block1_q, hash2 );
-    
-    always @ ( * ) begin
-    
-        if ( reset_q ) begin
-    
-            nonce_d = nonce_start;
-    
-            nonce_found_d = 1'b0;
-            nonce_out_d = 32'd0;
-            hash_out_d = 32'd0;
-    
-        end
-        else begin
-    
-            nonce_d = nonce_q + CORES;
-    
-            nonce_out_d = nonce_q - (CORES);
-            hash_out_d = { hash2[263:256], hash2[271:264], hash2[279:272], hash2[287:280] };
-    
-            if ( hash_out_d <= target_d )
-                nonce_found_d = 1'b1;
-            else
-                nonce_found_d = 1'b0;
-        
-        end
-    
-        block0_d = { block[639:32], nonce_q, 8'h80 };
-        block1_d = { hash1, 8'h80, 128'd0 };
-        target_d = block[31:0];    
-    
-        reset_d = reset;
-        
-    end
-    
-	always @ (posedge clk) begin
-    
-        block0_q <= block0_d;
-        block1_q <= block1_d;
-        target_q <= target_d;
-        
-        nonce_q <= nonce_d;
-        nonce_out_q <= nonce_out_d;
-        hash_out_q <= hash_out_d;
-    
-        nonce_found_q <= nonce_found_d;
-    
-        reset_q <= reset_d;
-        
- //        $display ("Hash1 : %x", hash1);
- //        $display ("Hash2 : %x", hash2);
-        $display("Nonce: %x, Hash: %x, Found: %d", nonce_out_d, hash_out_d, nonce_found_d );
-        
-    end
+	wire [511:0]block1,block2,block3;
+	wire [255:0] hash,out,constant,s2hash;
+	reg [31:0] nonce;
+	assign constant = 256'h5be0cd191f83d9ab9b05688c510e527fa54ff53a3c6ef372bb67ae856a09e667;
+	assign block1 = block[511:0];
+	assign block2 = {64'h240,319'b0,1'b1,nonce,block[607:512]};
+	assign block3 = {64'h100,191'b0,1'b1,hash};
+	sha256_transform s1(constant,block1,out);
+	sha256_transform s2(out,block2,s2hash);
+	sha256_transform s3(constant,block3,hash);
+    reg[31:0] target_d,nonce_out_d;
+    reg nonce_found_d;
+    assign nonce_found=nonce_found_d;
+    assign nonce_out=nonce;
+	always @ ( posedge clk ) begin
 
-endmodule
+		if ( reset ) begin
+			target_d = block[31:0];
+			nonce = nonce_start;
+			nonce_found_d = 1'b0;
+			nonce_out_d = 32'd0;
+
+		end
+		else begin
+			nonce = nonce + CORES;
+			if ( hash[255:224] <= target_d )
+				nonce_found_d = 1'b1;
+			else
+				nonce_found_d = 1'b0;
+		
+		end
+	end 
+	endmodule
