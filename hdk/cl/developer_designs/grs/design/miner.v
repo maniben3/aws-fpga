2a9642ef398c6fb6b6629ab2e689222d62bd04f4
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-08-21 01:44:13 +0530

    Update miner.v

--- hdk/cl/developer_designs/grs/design/miner.v
+++ hdk/cl/developer_designs/grs/design/miner.v
@@ -16,7 +16,7 @@ module miner # (
 	assign constant = 256'h5be0cd191f83d9ab9b05688c510e527fa54ff53a3c6ef372bb67ae856a09e667;
 	assign block1 = block[511:0];
 	assign block2 = {64'h240,319'b0,1'b1,nonce,block[607:512]};
-	assign block3 = {64'h100,191'b0,1'b1,hash};
+	assign block3 = {64'h100,191'b0,1'b1,s2hash};
 	sha256_transform s1(constant,block1,out);
 	sha256_transform s2(out,block2,s2hash);
 	sha256_transform s3(constant,block3,hash);
